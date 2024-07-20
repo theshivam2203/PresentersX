000ad06f2e4b89dadb35aede9aa9b6bda1169701
theshivam2203 <136744692+theshivam2203@users.noreply.github.com>
2024-07-20 17:26:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+mport os
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
@@ -16,7 +16,6 @@ cap.set(4, height)
 
 # Get the list of presentation images
 pathImages = sorted(os.listdir(folderPath), key=len)
-# print(pathImages)
 
 # Variables
 imgList = []
@@ -49,25 +48,25 @@ while True:
     imgCurrent = cv2.imread(pathFullImage)
 
     hands, img = detector.findHands(img)
-    cv2.line(img,(0, gestureThreshold), (width, gestureThreshold), (0, 255, 0), 10)
+    cv2.line(img, (0, gestureThreshold), (width, gestureThreshold), (0, 255, 0), 10)
 
-    if hands and buttonPressed is False:
+    if hands and not buttonPressed:
         hand = hands[0]
         fingers = detector.fingersUp(hand)
         cx, cy = hand['center']
         lmList = hand['lmList']
 
         # Constrain values for easier drawing
-        xVal = int(np.interp(lmList[8][0], [width//2, width], [0, width]))
-        yVal = int(np.interp(lmList[8][1], [150, height-150], [0, height]))
+        xVal = int(np.interp(lmList[8][0], [width // 2, width], [0, width]))
+        yVal = int(np.interp(lmList[8][1], [150, height - 150], [0, height]))
         indexFinger = xVal, yVal
 
-        if cy <= gestureThreshold: # if hand is at the height of the face
+        if cy <= gestureThreshold:  # if hand is at the height of the face
             annotationStart = False
             # Gesture 1 - Left
             if fingers == [1, 0, 0, 0, 0]:
                 annotationStart = False
-                print ("Left")
+                print("Left")
                 if imgNumber > 0:
                     buttonPressed = True
                     annotations = [[]]
@@ -78,7 +77,7 @@ while True:
             if fingers == [0, 0, 0, 0, 1]:
                 annotationStart = False
                 print("Right")
-                if imgNumber < len(pathImages)-1:
+                if imgNumber < len(pathImages) - 1:
                     buttonPressed = True
                     annotations = [[]]
                     annotationNumber = 0
@@ -86,12 +85,12 @@ while True:
 
         # Gesture 3 - Show Pointer
         if fingers == [0, 1, 1, 0, 0]:
-            cv2.circle(imgCurrent, indexFinger, 12,(0, 0, 255), cv2.FILLED)
+            cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
             annotationStart = False
 
         # Gesture 4 - Draw Pointer
         if fingers == [0, 1, 0, 0, 0]:
-            if annotationStart is False:
+            if not annotationStart:
                 annotationStart = True
                 annotationNumber += 1
                 annotations.append([])
@@ -133,28 +132,26 @@ while True:
                         command = recognizer.recognize_google(audio, language="en-in").lower()
                         print(f"Voice Command: {command}")
                         # Extract slide number from command
-                        slide_number = int(command.split("slide")[-1].strip())
-                        # Verify and navigate to the desired slide
-                        if 0 < slide_number <= len(pathImages):
-                            imgNumber = slide_number -1
-                            annotations = [[]]
-                            annotationNumber = -1
-                            annotationStart = False
-                            print(f"Going to slide {slide_number}")
-                            for path in pathImages:
-                                if (path == imgNumber):
-                                    cv2.imshow("Image", img)
-
+                        if "slide" in command:
+                            try:
+                                slide_number = int(command.split("slide")[-1].strip())
+                                # Verify and navigate to the desired slide
+                                if 0 < slide_number <= len(pathImages):
+                                    imgNumber = slide_number - 1
+                                    annotations = [[]]
+                                    annotationNumber = -1
+                                    annotationStart = False
+                                    print(f"Going to slide {slide_number}")
+                                else:
+                                    print("Invalid slide number")
+                            except ValueError:
+                                print("Could not understand the slide number command.")
                         else:
-                            print("Invalid slide number")
-                            exit()
-
+                            print("Command not recognized as a slide number.")
                 except sr.UnknownValueError:
                     print("Could not understand audio")
-
                 except sr.RequestError as e:
                     print(f"Could not request results from Google Speech Recognition service; {e}")
-                    exit()
 
     else:
         annotationStart = False
@@ -169,14 +166,14 @@ while True:
     for i in range(len(annotations)):
         for j in range(len(annotations[i])):
             if j != 0:
-                cv2.line(imgCurrent, annotations[i][j-1], annotations[i][j], (0, 0, 200), 12)
+                cv2.line(imgCurrent, annotations[i][j - 1], annotations[i][j], (0, 0, 200), 12)
 
     imgCurrent = cv2.resize(imgCurrent, None, fx=zoomScale, fy=zoomScale)
 
     # Adding Webcam image on the slides
     imgSmall = cv2.resize(img, (ws, hs))
     h, w, _ = imgCurrent.shape
-    imgCurrent[0:hs,w-ws:w] = imgSmall
+    imgCurrent[0:hs, w - ws:w] = imgSmall
 
     cv2.imshow("Image", img)
     cv2.imshow("Slides", imgCurrent)
